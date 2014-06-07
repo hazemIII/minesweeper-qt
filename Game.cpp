f19385e54dcde331ebe135e84efce252533d424c
Michal Katner <katnerster@gmail.com>
2014-06-07 22:38:45 +0200

    Added Game class. Updated .pro file.

---
+++ Game.cpp
@@ -0,0 +1,11 @@
+#include "Game.hpp"
+
+Game::Game()
+{
+
+}
+
+Game::~Game()
+{
+
+}
---
+++ Game.hpp
@@ -0,0 +1,15 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include <QObject>
+
+class Game : public QObject
+{
+  Q_OBJECT
+
+  public:
+    Game();
+    ~Game();
+};
+
+#endif
